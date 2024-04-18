8414c0e83aa4d6775ad9331094c34764b75a993a
basnetrajpradip <basnetrajpradip@gmail.com>
2024-04-18 12:57:34 +0545

    User schema model

---
+++ models/user.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    avatarId: { type: String, required: true },
+    password: {
+      type: String,
+      required: true,
+    },
+    messages: [
+      {
+        message: String,
+        sender: String,
+        reciver: String,
+        time: Date,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = models.User || mongoose.model("User", userSchema);
+export default User;
